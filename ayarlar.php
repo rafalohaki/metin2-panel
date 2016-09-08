c93156f5e0403e998331db73cf47f0496fdbe1f6
MMOTutkunlari <gs.eray.55@hotmail.com>
2016-09-08 22:36:18 +0300

    Update ayarlar.php

--- ayarlar.php
+++ ayarlar.php
@@ -1,1 +1,12 @@
+<?php
+//server bilgileri
+define("SERVER_IP", "xxx.xxx.xxx.xxxx") // bu bölüme server ipniz gelecek.
+define("MYSQL_USER", "root") //bu bölüme mysql kullanıcı adınız.
+define("MYSQL_PASS", "") //bu bölüme mysql şifreniz.
+define("MYSQL_DB", "account") //burası kalsın
 
+
+
+
+
+?>
