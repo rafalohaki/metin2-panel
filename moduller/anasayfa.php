2ee81e484f68805e4b424b1ca31032c9703b87d0
MMOTutkunlari <gs.eray.55@hotmail.com>
2016-09-08 23:38:59 +0300

    Update anasayfa.php

--- moduller/anasayfa.php
+++ moduller/anasayfa.php
@@ -1,1 +1,20 @@
+<?php
+if(GUVENLIK == true) { die("Sadece index dosyasından giriniz.")}
 
+class anasayfa.php
+{
+  public $title = "Anasayfa";
+  
+  public function css()
+  {}
+  
+  public function js()
+  {}
+  
+  public function icerik()
+  {
+    echo "Aanasayfaya hoş geldiniz.";
+  }
+}
+
+?>
