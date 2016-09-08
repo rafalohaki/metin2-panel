781ce90199f66b82ea431a32de801b9d2c597af1
MMOTutkunlari <gs.eray.55@hotmail.com>
2016-09-08 22:40:31 +0300

    Update fonksiyonlar.php

--- fonksiyonlar.php
+++ fonksiyonlar.php
@@ -1,1 +1,19 @@
+<?php
 
+//ayar dosyamızı içeri alalım.
+require_once('ayarlar.php');
+
+//klasör yapılarımızın sabitleri
+define("KLASOR_CLASSLARIM", "classlarim/");
+define("KLASOR_MODULLER", "moduller/");
+define("KLASOR_TEMA", "temalar/varsayilan");
+
+//classlarimizi çekelim
+require_once KLASOR_CLASSLARIM.Veritabani.php;
+require_once KLASOR_CLASSLARIM.Guvenlik.php;
+require_once KLASIR_CLASSLARIM.Fonksiyonlar.php;
+
+//nesneleri oluştur.
+$db = new Veritabani;
+$gvn = new Guvenlik;
+$fonk = new Fonksiyonlar;
