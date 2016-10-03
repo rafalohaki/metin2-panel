e3abaa2c227ffb9bc1c343636d770db3adcb3582
MMOTutkunlari <gs.eray.55@hotmail.com>
2016-10-03 16:24:11 +0300

    Update tema.php

--- classlarim/tema.php
+++ classlarim/tema.php
@@ -1,1 +1,19 @@
 
+<?php
+class Tema
+{
+  protected $temaKlasor = "temalar/";
+  protected $gecerliTema = SITE_TEMASI;
+  protected $defaultTema = "default";
+  
+  function __construct()
+  {
+  }
+  
+  private function temaAl()
+  {
+    $temaYol = $temaKlasor.$gecerliTema;
+    
+  }
+}
+?>
