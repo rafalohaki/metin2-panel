7ac4571a4868f6635de0a3ec5363a1a6d8fda46a
MMOTutkunlari <gs.eray.55@hotmail.com>
2016-10-03 16:39:48 +0300

    Update tema.php

--- classlarim/tema.php
+++ classlarim/tema.php
@@ -5,6 +5,8 @@ class Tema
   protected $temaKlasor = "temalar/";
   protected $gecerliTema = SITE_TEMASI;
   protected $defaultTema = "default";
+  protected $cssYol = "css/";
+  protected $jsYol = "js/";
   
   function __construct()
   {
@@ -13,7 +15,27 @@ class Tema
   private function temaAl()
   {
     $temaYol = $temaKlasor.$gecerliTema;
-    
+    if(is_dir($temaYol))
+    {
+      return $gecerliTema;
+    }
+    else
+    {
+      return $defaultTema;
+    }
+  }
+  
+  private function css_al($cssDosyasi)
+  {
+    $cssYol2 = $temaKlasor.$this->temaAl().$cssYol;
+    $cssYolFile = $temaKlasor.$this->temaAl().$cssYol.$cssDosyasi;
+    if(is_dir($cssYol2))
+    {
+      if(file_exists($cssYolFile))
+      {
+        return $cssYolFile;
+      }
+    }
   }
 }
 ?>
