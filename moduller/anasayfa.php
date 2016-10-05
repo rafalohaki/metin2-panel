1d6c22a84d23a411c49d8cae072caff56da8ccfe
MMOTutkunlari <gs.eray.55@hotmail.com>
2016-10-05 10:54:29 +0300

    Update anasayfa.php

--- moduller/anasayfa.php
+++ moduller/anasayfa.php
@@ -3,12 +3,15 @@ if(GUVENLIK == true) { die("Sadece index dosyasından giriniz.")}
 
 class anasayfa.php
 {
+  global $tema;
   public $title = "Anasayfa";
   
-  public function css()
-  {}
+  public function extra_css()
+  {
+    ?><link rel="stylesheet" href="<?=$tema->css_al("deneme_extra.css");?>"><?php
+  }
   
-  public function js()
+  public function extra_js()
   {}
   
   public function icerik()
